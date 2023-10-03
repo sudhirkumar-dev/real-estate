bd052302efccb48def04858ee549b73cafc54bb1
sudhirkumar-dev <kalivarapuskumar@gmail.com>
2023-10-03 15:09:40 +0530

    created signup ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 // import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import SignOut from "./pages/SignOut";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
@@ -14,7 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-red-500">Quick</span>
-            <span className="text-slate-500">estate</span>
+            <span className="text-slate-500">Estate</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
