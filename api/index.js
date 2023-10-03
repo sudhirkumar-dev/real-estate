08615dac3adfc4a7e0bbf94ea1c7be4b29567e39
sudhirkumar-dev <kalivarapuskumar@gmail.com>
2023-10-03 14:40:58 +0530

    error middleware

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route.js';
-import authRouter from './routes/auth.route.js';
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -16,12 +16,20 @@ mongoose
   });
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is on port 3000!!");
 });
 
-
-app.use("/api/user",userRouter);
-app.use("/api/auth",authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: message,
+  });
+});
