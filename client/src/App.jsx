f4f8288a72aced2b2ea69f186b66880f5363c73d
sudhirkumar-dev <kalivarapuskumar@gmail.com>
2023-10-17 18:22:36 +0530

    create listing pafe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import SignUp from "./pages/SignUp";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
-
+import Listing from "./pages/Listing";
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,6 +19,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing/>}/>
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
---
+++ client/src/pages/Listing.jsx
@@ -0,0 +1,10 @@
+// import { useEffect } from "react"
+
+// import React from 'react'
+
+
+export default function Listing() {
+  return (
+    <div>Listing</div>
+  )
+}
