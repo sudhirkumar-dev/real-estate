bab2af9471eff5cf21a0a7cad8daee72b4960f50
sudhirkumar-dev <kalivarapuskumar@gmail.com>
2023-10-22 20:31:25 +0530

    for deploy

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
-import listingRouter from './routes/listing.route.js'
+import listingRouter from "./routes/listing.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -16,10 +17,12 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+  const __dirname = path.resolve()
 const app = express();
 
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is on port 3000!!");
@@ -27,7 +30,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
-app.use("/api/listing",listingRouter)
+app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+})
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
